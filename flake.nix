d8bd702a3be6ac0900834c9143ccb84100a632f1
Josh Kingsley <josh@joshkingsley.me>
2021-11-16 02:03:46 +0200

    Remove 1Password module

--- flake.nix
+++ flake.nix
@@ -2,12 +2,11 @@
   description = "Nix systems configuration";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-21.05;
-    nixpkgs-unstable.url = github:nixos/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
     nixos-hardware.url = github:nixos/nixos-hardware;
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, nixos-hardware, ... }: {
+  outputs = { nixpkgs, nixos-hardware, ... }: {
     nixosConfigurations = {
       sparrowhawk = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
--- modules/1password.nix
+++
@@ -1,9 +0,0 @@
-nixpkgs-unstable: { ... }:
-
-{
-  nixpkgs.overlays = [
-    final: prev: {
-      _1password-gui = final.callPackage nixpkgs-unstable._1password-gui {};
-    }
-  ];
-}
