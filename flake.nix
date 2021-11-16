bbecbe14464c2207c2cd31de665d04f1a7522b4c
Josh Kingsley <josh@joshkingsley.me>
2021-11-16 02:19:20 +0200

    Reorganize

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix systems configuration";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-21.05;
     nixos-hardware.url = github:nixos/nixos-hardware;
   };
 
@@ -11,8 +11,9 @@
       sparrowhawk = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ 
-          ./hosts/sparrowhawk 
           nixos-hardware.nixosModules.lenovo-thinkpad-e14-intel 
+          ./modules/hosts/sparrowhawk 
+          ./modules/nix-unstable
         ];
       };
     };
