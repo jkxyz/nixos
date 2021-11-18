300185522226a557db74bac7fd4b02e9182b1879
Josh Kingsley <josh@joshkingsley.me>
2021-11-18 20:01:47 +0200

    Change doom config

--- modules/home/emacs/doom/org.el
+++ modules/home/emacs/doom/org.el
@@ -1,6 +1,13 @@
 ;;; $DOOMDIR/org.el -*- lexical-binding: t; -*-
 
+(require 'org)
+
 (setq org-directory "~/Notes/")
+
+(require 'org-journal)
+
+(setq org-journal-dir (concat org-directory "Journal/"))
+(setq org-journal-date-format "%A, %d %B %Y")
 
 (setq org-default-notes-file (concat org-directory "Inbox.org"))
 
@@ -11,15 +18,17 @@
 (setq org-mobile-directory "~/OrgMobile/")
 (setq org-mobile-inbox-for-pull (concat org-directory "Inbox (Mobile).org"))
 
-(use-package! org-roam
-  :custom
-  (org-roam-directory (concat org-directory "Roam/"))
-  (org-roam-dailies-directory "Journal/"))
+;; (use-package! org-roam
+;;   :custom
+;;   (org-roam-directory (concat org-directory "Roam/"))
+;;   (org-roam-dailies-directory "Journal/"))
 
-(add-to-list 'org-agenda-files org-roam-directory)
+;; (add-to-list 'org-agenda-files org-roam-directory)
 
 (setq org-todo-keywords
       '((sequence "TODO(t)" "|" "DONE(d!)")))
+
+(require 'org-capture)
 
 (setq org-capture-templates
       '(("t" "todo" plain (file org-default-notes-file)
@@ -32,26 +41,26 @@
            ((org-agenda-overriding-header "To refile")
             (org-tags-match-list-sublevels nil)))))))
 
-(require 'vulpea)
+;; (require 'vulpea)
 
-(defun jk/org-agenda-buffer-category ()
-  "Get category of item at point for agenda."
-  (let* ((file-name (when buffer-file-name
-                      (file-name-sans-extension
-                       (file-name-nondirectory buffer-file-name))))
-         (title (vulpea-buffer-prop-get "title"))
-         (category (org-get-category)))
-    (or (if (and title (string-equal category file-name))
-            title
-          category)
-        "")))
+;; (defun jk/org-agenda-buffer-category ()
+;;   "Get category of item at point for agenda."
+;;   (let* ((file-name (when buffer-file-name
+;;                       (file-name-sans-extension
+;;                        (file-name-nondirectory buffer-file-name))))
+;;          (title (vulpea-buffer-prop-get "title"))
+;;          (category (org-get-category)))
+;;     (or (if (and title (string-equal category file-name))
+;;             title
+;;           category)
+;;         "")))
 
-(setq org-agenda-prefix-format
-      '((agenda . " %i %-12(jk/org-agenda-buffer-category)%?-12t% s ")
-        (todo . " %i %-12(jk/org-agenda-buffer-category) ")
-        (tags  . " %i %-12(jk/org-agenda-buffer-category) ")
-        (search . " %i %-12(jk/org-agenda-buffer-category) ")))
+;; (setq org-agenda-prefix-format
+;;       '((agenda . " %i %-12(jk/org-agenda-buffer-category)%?-12t% s ")
+;;         (todo . " %i %-12(jk/org-agenda-buffer-category) ")
+;;         (tags  . " %i %-12(jk/org-agenda-buffer-category) ")
+;;         (search . " %i %-12(jk/org-agenda-buffer-category) ")))
 
-(require 'org-roam-dailies)
+;; (require 'org-roam-dailies)
 
-(add-to-list 'org-refile-targets `(,(concat org-roam-directory org-roam-dailies-directory) :maxlevel . 3))
+;; (add-to-list 'org-refile-targets `(,(concat org-roam-directory org-roam-dailies-directory) :maxlevel . 3))
