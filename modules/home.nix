3d0a6a35771cac52498fad1370bc3d4e3ff79ee6
Josh Kingsley <josh@joshkingsley.me>
2021-11-18 22:37:39 +0200

    Install spotify

--- modules/home.nix
+++ modules/home.nix
@@ -2,12 +2,12 @@
 
 {
   nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [ "slack" "1password" ];
+    builtins.elem (lib.getName pkg) [ "slack" "1password" "spotify" "spotify-unwrapped" ];
 
   home-manager.users.josh = { pkgs, ... }: {
     imports = [ ./home/emacs.nix ./home/development.nix ./home/syncthing.nix ];
 
-    home.packages = with pkgs; [ slack _1password-gui ];
+    home.packages = with pkgs; [ slack _1password-gui spotify ];
 
     programs.bash.enable = true;
 
