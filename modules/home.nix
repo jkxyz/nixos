6777022cf8002c7ae095639306153fe4517f67b4
Josh Kingsley <josh@joshkingsley.me>
2021-11-18 20:09:56 +0200

    Add slack and 1password

--- modules/home.nix
+++ modules/home.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ lib, ... }:
 
 {
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [ "slack" "1password" ];
+
   home-manager.users.josh = { pkgs, ... }: {
     imports = [ ./home/emacs.nix ./home/development.nix ./home/syncthing.nix ];
+
+    home.packages = with pkgs; [ slack _1password-gui ];
 
     programs.bash.enable = true;
 
