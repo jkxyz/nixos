90310fbf274e36194760ada2195e02ebf285db1a
Josh Kingsley <josh@joshkingsley.me>
2022-07-24 17:48:27 +0300

    Enable printing

--- modules/hosts/sparrowhawk/configuration.nix
+++ modules/hosts/sparrowhawk/configuration.nix
@@ -143,6 +143,12 @@ in {
 
   networking.firewall.checkReversePath = "loose";
 
+  services.printing.enable = true;
+  services.printing.drivers = [ pkgs.hplip ];
+
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
