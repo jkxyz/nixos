46d3ef6d1566c6ae2fda8d63a88d4234c2f8ec36
Josh Kingsley <josh@joshkingsley.me>
2022-09-27 13:22:50 +0300

    Upgrade

--- modules/home/clojure.nix
+++ modules/home/clojure.nix
@@ -2,4 +2,9 @@
 
 {
   home.file.".clojure/deps.edn".source = ./clojure/deps.edn;
+  home.packages = [
+    pkgs.unstable.clojure
+    pkgs.unstable.clojure-lsp
+    pkgs.unstable.clj-kondo
+  ];
 }
