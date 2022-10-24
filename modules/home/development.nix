2a8b1c13eed266ffcc8abc239154a1aae472c3b0
Josh Kingsley <josh@joshkingsley.me>
2022-10-24 21:37:06 +0300

    Changes

--- modules/home/development.nix
+++ modules/home/development.nix
@@ -4,5 +4,8 @@
   home.packages = with pkgs; [
     unstable.nodejs
     unstable.postgresql
+    unstable.rustc
+    unstable.cargo
+    unstable.rust-analyzer
   ];
 }
--- modules/home/emacs/doom/custom.el
+++ modules/home/emacs/doom/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
+   '("246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
  '(package-selected-packages '(graphql-mode evil-cleverparens smartparens))
  '(safe-local-variable-values
    '((eval put 'defnc 'clojure-doc-string-elt 2)
