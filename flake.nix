58731470ae59f25eff0075919212355172d65810
Josh Kingsley <josh@joshkingsley.me>
2023-07-04 13:39:55 +0300

    Add Firefox nightly package for testing

--- flake.nix
+++ flake.nix
@@ -4,19 +4,27 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-emacs.url = "github:nixos/nixpkgs?rev=c90c4025bb6e0c4eaf438128a3b2640314b1c58d";
+    nixpkgs-emacs.url =
+      "github:nixos/nixpkgs?rev=c90c4025bb6e0c4eaf438128a3b2640314b1c58d";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-mozilla.url = "github:mozilla/nixpkgs-mozilla";
   };
 
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixpkgs-emacs
-    , nixos-hardware, ... }:
+    , nixos-hardware, nixpkgs-mozilla, ... }:
     let pkgs = import nixpkgs { system = "x86_64-linux"; };
     in {
       devShell.x86_64-linux =
         pkgs.mkShell { buildInputs = [ pkgs.nvd pkgs.babashka ]; };
+      packages.x86_64-linux.firefox-nightly-bin = let
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          overlays = [ nixpkgs-mozilla.overlays.firefox ];
+        };
+      in pkgs.latest.firefox-nightly-bin;
       nixosConfigurations = {
         sparrowhawk = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
