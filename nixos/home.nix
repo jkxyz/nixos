926b7573f9741f24cd75649a825c3b569c83f442
Josh Kingsley <josh@joshkingsley.me>
2023-07-28 00:25:05 +0300

    Start re-factoring modules

---
+++ nixos/home.nix
@@ -0,0 +1,34 @@
+{ lib, inputs, config, ... }:
+
+{
+  options = {
+    jkxyz.home.users.josh.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = lib.mdDoc ''
+        Enable the home-manager modules for desktop user `josh`.
+      '';
+    };
+
+    jkxyz.home.users.josh.extraGroups = lib.mkOption {
+      type = lib.types.listOf lib.types.string;
+      default = [ ];
+    };
+  };
+
+  imports = [ inputs.home-manager.nixosModules.home-manager ];
+
+  config = lib.mkMerge [
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+    }
+
+    (lib.mkIf config.jkxyz.home.users.josh.enable {
+      users.users.josh = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ] ++ config.jkxyz.home.users.josh.extraGroups;
+      };
+    })
+  ];
+}
--- nixos/nix.nix
+++ nixos/nix.nix
@@ -1,13 +1,23 @@
-{ ... }:
+{ lib, config, ... }:
 
 {
-  # Reference: https://nixos.org/manual/nix/stable/command-ref/conf-file.html
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-
-    # Don't gc outputs to speed up dev shell evaluation
-    # TODO Enable this under a custom NixOS option
-    keep-outputs = true;
-    keep-derivations = true;
+  options = {
+    jkxyz.nix.persistDerivations = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = lib.mdDoc ''
+        Whether to enable setting keep-derivations and keep-outputs to true to prevent shells from getting garbage collected.
+      '';
+    };
   };
+
+  config = {
+    nix.settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+    } // lib.mkIf config.jkxyz.nix.persistDerivations {
+      keep-outputs = true;
+      keep-derivations = true;
+    };
+  };
+
 }
