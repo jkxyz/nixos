d2b4e9755b06e1042efaac29a142bdb03271f762
Josh Kingsley <josh@joshkingsley.me>
2023-12-11 15:39:42 +0200

    Upgrade NixOS

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixpkgs-mozilla.url = "github:mozilla/nixpkgs-mozilla";
