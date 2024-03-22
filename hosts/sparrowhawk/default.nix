f80669c02832fe18733b0c89dede7eb1389538d4
Josh Kingsley <josh@joshkingsley.me>
2024-03-22 12:03:03 +0200

    Disable automatic-timezoned

--- hosts/sparrowhawk/default.nix
+++ hosts/sparrowhawk/default.nix
@@ -55,8 +55,6 @@ in {
   # time.timeZone = "Europe/London";
   # time.timeZone = "America/Los_Angeles";
 
-  services.automatic-timezoned.enable = true;
-
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
